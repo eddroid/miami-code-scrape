1beefa6c17d4820d10ac5b75dbbb1b02353e6ec7
Ed Toro <ed@inversiones.com>
2014-01-13 20:31:02 -0500

    big refactoring: separate scraper from parser
    - add a README
    - move casper.js to scraper.js
    - remove old data dumps
    - generate a scrape.json data dump file
    - add stub for structure.js

---
+++ README.md
@@ -0,0 +1,15 @@
+Install phantomjs on Mac with HomeBrew
+----
+
+1. '''update brew'''
+1. '''brew install casperjs --devel'''
+
+Run scraper to generate scrape.json
+----
+
+'''casperjs scraper.js --verbose'''
+
+Figure out the hierarchy structure of the pages based on the (poorly formatted) Table of Contents
+----
+
+'''node structure.js'''
---
+++ scraper.js
@@ -0,0 +1,52 @@
+var utils = require('utils'),
+    fs = require('fs'),
+    casper = require('casper').create({
+      verbose: true,
+      logLevel: 'debug',
+      pageSettings: {
+        loadImages: false,
+      loadPlugins: false
+      },
+      clientScripts: []
+    }),
+    table = [];
+
+casper.on("remote.message", function(message) {
+  this.echo("[console.log] " + message);
+});
+
+// get top level links
+function getLinks () {
+  var links = $('a');
+  return [].map.call(links, function(link) {
+    return link.href;
+  });
+}
+
+function onLink(link) {
+  this.echo(link);
+  this.thenOpen(link, function() {
+    var row = {
+      url: link,
+      title: this.fetchText('title'),
+      html: this.getHTML()
+    };
+    table.push(row);
+  });
+}
+
+casper.start('http://library.municode.com/toc.aspx?clientId=10620&checks=false', function() {
+    links = this.evaluate(getLinks);
+
+    this.echo(links.length + ' links found');
+    for (var i = 0, link; i < links.length; i++) {
+      link = links[i];
+      onLink.call(this, link);
+    }
+    });
+
+casper.then(function() {
+  fs.write("scrape.json", JSON.stringify(table), 'w');
+});
+
+casper.run();
